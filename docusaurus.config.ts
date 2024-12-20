966ea947ed8fc31d9d5e6eb6ff7f0ec5fa814f36
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2024-12-20 18:51:26 +0100

    search bar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -125,6 +125,9 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+  plugins: [
+    require.resolve('docusaurus-lunr-search')
+  ],
 };
 
 export default config;
